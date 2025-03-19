49a7b53b6575a403e4ea0440f7b566ff3510f38f
Sajid-tech <s.khan9430319425@gmail.com>
2025-03-19 18:37:09 +0530

    `purchase done`

--- src/app/purchase/PurchaseList.jsx
+++ src/app/purchase/PurchaseList.jsx
@@ -73,6 +73,7 @@ const PurchaseList = () => {
   const [columnFilters, setColumnFilters] = useState([]);
   const [columnVisibility, setColumnVisibility] = useState({});
   const [rowSelection, setRowSelection] = useState({});
+  const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
 
   // Define columns for the table
@@ -115,11 +116,10 @@ const PurchaseList = () => {
 
         return (
           <span
-            className={`px-2 py-1 rounded text-xs ${
-              status == "Active"
+            className={`px-2 py-1 rounded text-xs ${status == "Active"
                 ? "bg-green-100 text-green-800"
                 : "bg-gray-100 text-gray-800"
-            }`}
+              }`}
           >
             {status}
           </span>
@@ -141,7 +141,7 @@ const PurchaseList = () => {
                     variant="ghost"
                     size="icon"
                     onClick={() => {
-                      navigateToPurchaseEdit(navigate,purchaseId)
+                      navigateToPurchaseEdit(navigate, purchaseId)
                     }}
                   >
                     <Edit />
@@ -157,6 +157,9 @@ const PurchaseList = () => {
       },
     },
   ];
+  const filteredItems = purchase?.filter((item) =>
+    item.purchase_buyer_name.toLowerCase().includes(searchQuery.toLowerCase())
+  ) || [];
 
   // Create the table instance
   const table = useReactTable({
@@ -188,7 +191,7 @@ const PurchaseList = () => {
     return (
       <Page>
         <div className="flex justify-center items-center h-full">
-         <Loader/>
+          <Loader />
         </div>
       </Page>
     );
@@ -216,137 +219,293 @@ const PurchaseList = () => {
 
   return (
     <Page>
-      <div className="w-full p-4 grid grid-cols-1">
-        <div className="flex text-left text-2xl text-gray-800 font-[400]">
-          Purchase List
-        </div>
-
-        <div className="flex flex-col md:flex-row md:items-center py-4 gap-2">
-          {/* Search Input */}
-          <div className="relative w-full md:w-72">
-            <Search className="absolute left-2 top-2.5 h-4 w-4 text-gray-500" />
-            <Input
-              placeholder="Search Purchase..."
-              value={table.getState().globalFilter || ""}
-              onChange={(event) => table.setGlobalFilter(event.target.value)}
-              className="pl-8 bg-gray-50 border-gray-200 focus:border-gray-300 focus:ring-gray-200 w-full"
-            />
+      <div className="w-full p-0 md:p-4 grid grid-cols-1">
+        <div className="sm:hidden">
+          <div className="flex justify-between items-center mb-4">
+            <h1 className="text-xl md:text-2xl text-gray-800 font-medium">
+              Purchase List
+            </h1>
+            <div>
+              <Button
+                variant="default"
+                className={`md:ml-2 bg-yellow-400 hover:bg-yellow-600 text-black rounded-l-full`}
+                onClick={() => {
+                  navigate("/purchase/create");
+                }}
+              >
+                <SquarePlus className="h-4 w-4 " /> Purchase
+              </Button>
+            </div>
           </div>
 
-          {/* Dropdown Menu & Sales Button */}
-          <div className="flex flex-col md:flex-row md:ml-auto gap-2 w-full md:w-auto">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="w-full md:w-auto">
-                  Columns <ChevronDown className="ml-2 h-4 w-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                {table
-                  .getAllColumns()
-                  .filter((column) => column.getCanHide())
-                  .map((column) => (
-                    <DropdownMenuCheckboxItem
-                      key={column.id}
-                      className="capitalize"
-                      checked={column.getIsVisible()}
-                      onCheckedChange={(value) =>
-                        column.toggleVisibility(!!value)
-                      }
-                    >
-                      {column.id}
-                    </DropdownMenuCheckboxItem>
-                  ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <Button
-              variant="default"
-              className={`ml-2 ${ButtonConfig.backgroundColor} ${ButtonConfig.hoverBackgroundColor} ${ButtonConfig.textColor}`}
-              onClick={() => {
-                navigate("/purchase/create");
-              }}
-            >
-              <SquarePlus className="h-4 w-4 mr-2" /> Purchase
-            </Button>{" "}
+          <div className="flex flex-col md:flex-row md:items-center py-4 gap-2">
+            {/* Search Input */}
+            <div className="relative w-full md:w-72">
+              <Search className="absolute left-2 top-2.5 h-4 w-4 text-gray-500" />
+              <Input
+                placeholder="Search item..."
+                value={searchQuery}
+                onChange={(event) => setSearchQuery(event.target.value)}
+                className="pl-8 bg-gray-50 border-gray-200 focus:border-gray-300 focus:ring-gray-200 w-full"
+              />
+            </div>
+          </div>
+
+          <div className="space-y-3">
+            {filteredItems.length > 0 ? (
+              filteredItems.map((item, index) => (
+                <div
+                  key={item.id}
+                  className="relative bg-white rounded-lg shadow-sm border-l-4 border-r border-b border-t border-yellow-500 overflow-hidden"
+                >
+                  <div className="p-2 flex flex-col gap-2">
+                    {/* Sl No and Item Name */}
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center space-x-2">
+                        <div className="bg-gray-100 text-gray-600 rounded-full w-4 h-4 flex items-center justify-center text-xs font-medium">
+                          {index + 1}
+                        </div>
+                        <h3 className="font-medium text-sm text-gray-800">
+                          {item.purchase_buyer_name}
+                        </h3>
+                      </div>
+                      <div className="flex items-center justify-between gap-2 ">
+                        <span
+                          className={`px-2 py-1 rounded-full text-xs font-medium ${item.purchase_status === "Active"
+                            ? "bg-green-100 text-green-800"
+                            : "bg-gray-100 text-gray-800"
+                            }`}
+                        >
+                          {item.purchase_status}
+                        </span>
+                        <Button
+                          variant="ghost"
+                          size="icon"
+                          onClick={() => {
+                            navigateToPurchaseEdit(navigate, item.id)
+                          }}
+                        >
+                          <Edit />
+                        </Button>
+                        {/* <EditItem ItemId={} /> */}
+                      </div>
+                    </div>
+
+
+                    <div className="flex flex-wrap justify-between gap-1">
+                      {item.purchase_ref_no && (
+                        <div className="inline-flex items-center bg-gray-100 rounded-full px-2 py-1">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="10"
+                            height="10"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="text-gray-600 mr-1"
+                          >
+                            <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
+                          </svg>
+                          <span className="text-xs text-gray-700"><span className="text-[10px]">Ref No:</span>{item.purchase_ref_no}</span>
+                        </div>
+                      )}
+                      {item.purchase_vehicle_no && (
+                        <div className="inline-flex items-center bg-gray-100 rounded-full px-2 py-1">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="10"
+                            height="10"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="text-gray-600 mr-1"
+                          >
+                            <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" />
+                            <path d="M13 5v2" />
+                            <path d="M13 17v2" />
+                            <path d="M13 11v2" />
+                          </svg>
+                          <span className="text-xs text-gray-700"><span className="text-[10px]">Vehicle No:</span>{item.purchase_vehicle_no}</span>
+                        </div>
+                      )}
+                      {item.purchase_date && (
+
+
+                        <div className="inline-flex items-center bg-gray-100 rounded-full px-2 py-1">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="10"
+                            height="10"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="text-gray-600 mr-1"
+                          >
+                            <rect width="18" height="18" x="3" y="4" rx="2" ry="2" />
+                            <line x1="16" y1="2" x2="16" y2="6" />
+                            <line x1="8" y1="2" x2="8" y2="6" />
+                            <line x1="3" y1="10" x2="21" y2="10" />
+                          </svg>
+                          <span className="text-xs text-gray-700">
+                            {moment(item.purchase_date).format("DD-MMM-YY")}
+                          </span>
+                        </div>
+                      )}
+                    </div>
+
+
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="bg-white rounded-lg shadow-sm border border-gray-100 p-6 text-center text-gray-500">
+                No items found.
+              </div>
+            )}
           </div>
         </div>
-        {/* table  */}
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead
-                        key={header.id}
-                        className={` ${ButtonConfig.tableHeader} ${ButtonConfig.tableLabel}`}
+
+        <div className="hidden sm:block">
+          <div className="flex text-left text-2xl text-gray-800 font-[400]">
+            Purchase List
+          </div>
+
+          <div className="flex flex-col md:flex-row md:items-center py-4 gap-2">
+            {/* Search Input */}
+            <div className="relative w-full md:w-72">
+              <Search className="absolute left-2 top-2.5 h-4 w-4 text-gray-500" />
+              <Input
+                placeholder="Search Purchase..."
+                value={table.getState().globalFilter || ""}
+                onChange={(event) => table.setGlobalFilter(event.target.value)}
+                className="pl-8 bg-gray-50 border-gray-200 focus:border-gray-300 focus:ring-gray-200 w-full"
+              />
+            </div>
+
+            {/* Dropdown Menu & Sales Button */}
+            <div className="flex flex-col md:flex-row md:ml-auto gap-2 w-full md:w-auto">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" className="w-full md:w-auto">
+                    Columns <ChevronDown className="ml-2 h-4 w-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  {table
+                    .getAllColumns()
+                    .filter((column) => column.getCanHide())
+                    .map((column) => (
+                      <DropdownMenuCheckboxItem
+                        key={column.id}
+                        className="capitalize"
+                        checked={column.getIsVisible()}
+                        onCheckedChange={(value) =>
+                          column.toggleVisibility(!!value)
+                        }
                       >
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
+                        {column.id}
+                      </DropdownMenuCheckboxItem>
+                    ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <Button
+                variant="default"
+                className={`ml-2 ${ButtonConfig.backgroundColor} ${ButtonConfig.hoverBackgroundColor} ${ButtonConfig.textColor}`}
+                onClick={() => {
+                  navigate("/purchase/create");
+                }}
+              >
+                <SquarePlus className="h-4 w-4 mr-2" /> Purchase
+              </Button>{" "}
+            </div>
+          </div>
+          {/* table  */}
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <TableHead
+                          key={header.id}
+                          className={` ${ButtonConfig.tableHeader} ${ButtonConfig.tableLabel}`}
+                        >
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
                               header.column.columnDef.header,
                               header.getContext()
                             )}
-                      </TableHead>
-                    );
-                  })}
-                </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && "selected"}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </TableCell>
-                    ))}
+                        </TableHead>
+                      );
+                    })}
                   </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className="h-24 text-center"
-                  >
-                    No results.
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        {/* row slection and pagintaion button  */}
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="flex-1 text-sm text-muted-foreground">
-            Total Purchase : &nbsp;
-            {table.getFilteredRowModel().rows.length}
+                ))}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      data-state={row.getIsSelected() && "selected"}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={columns.length}
+                      className="h-24 text-center"
+                    >
+                      No results.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
           </div>
-          <div className="space-x-2">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              Previous
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              Next
-            </Button>
+          {/* row slection and pagintaion button  */}
+          <div className="flex items-center justify-end space-x-2 py-4">
+            <div className="flex-1 text-sm text-muted-foreground">
+              Total Purchase : &nbsp;
+              {table.getFilteredRowModel().rows.length}
+            </div>
+            <div className="space-x-2">
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
+              >
+                Previous
+              </Button>
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.nextPage()}
+                disabled={!table.getCanNextPage()}
+              >
+                Next
+              </Button>
+            </div>
           </div>
         </div>
       </div>
