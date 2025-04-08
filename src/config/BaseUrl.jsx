4af80d0925ed472f12e6fc8d4a96e6c5db8d6812
Sajid-tech <s.khan9430319425@gmail.com>
2025-04-08 18:46:35 +0530

    ` changed the base url`

--- src/config/BaseUrl.jsx
+++ src/config/BaseUrl.jsx
@@ -1,4 +1,5 @@
 //ace
-const BASE_URL = "https://test.dfclogistics.online/public";
+// const BASE_URL = "https://test.dfclogistics.online/public";
+const BASE_URL = "https://elgresceramic.com/stockapi/public";
 
 export default BASE_URL;
