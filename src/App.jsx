48c3ce496972b749c83050d6f41d9a1997aa7332
Moorthy <moorthy.chandiran21@gmail.com>
2025-05-09 11:52:03 +0530

    chnages done as per the condition

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   };
   return (
     <>
-      {/* <DisabledRightClick /> */}
+      <DisabledRightClick />
       <Toaster />
       <SessionTimeoutTracker expiryTime={time} onLogout={handleLogout} />
       <AppRoutes />
